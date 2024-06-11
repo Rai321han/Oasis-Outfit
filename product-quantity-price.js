f5cee04f5851b0042a4bfe23b3d93338f0c377ae
Raihan Uddin <uddinraihan797@gmail.com>
2024-06-11 21:32:54 +0600

    Quantity Price removes

--- product-quantity-price.js
+++ product-quantity-price.js
@@ -1,25 +1,20 @@
 let spinNumberOutput = document.querySelector(".spinNumberOutput");
-let regularPrice = document.querySelector(".regularPrice");
+// let regularPrice = document.querySelector(".regularPrice");
 let quantityOutput = document.querySelector(".quantityOutput");
 let plusButton = document.querySelector(".incrimentButton");
 let minusButton = document.querySelector(".decrimentButton");
 
 spinNumberOutput.value = 1;
-quantityOutput.innerHTML = regularPrice.innerHTML * spinNumberOutput.value;
+// quantityOutput.innerHTML = regularPrice.innerHTML * spinNumberOutput.value;
 
 plusButton.addEventListener("click", function () {
   spinNumberOutput.value++;
-  console.log(
-    (quantityOutput.innerHTML = regularPrice.innerHTML * spinNumberOutput.value)
-  );
+  // quantityOutput.innerHTML = regularPrice.innerHTML * spinNumberOutput.value;
 });
 
 minusButton.addEventListener("click", function () {
   if (spinNumberOutput.value > 1) {
     spinNumberOutput.value--;
-    console.log(
-      (quantityOutput.innerHTML =
-        regularPrice.innerHTML * spinNumberOutput.value)
-    );
+    // quantityOutput.innerHTML = regularPrice.innerHTML * spinNumberOutput.value;
   }
 });
